2fd47e7c46354b26f8fa4f0df41c02ce5c7b5897
Jacob <schoen575@gmail.com>
2015-09-01 11:58:16 -0400

    Chapter 1 - The Basics

---
+++ basics.rb
@@ -0,0 +1,51 @@
+first_name = "Joe"
+last_name = "Schoen"
+
+puts "#{first_name} #{last_name}"
+
+#challenge 2
+
+n = 6543
+
+puts n / 1000
+puts n % 1000 / 100
+puts n % 1000 % 100 / 10
+puts n % 1000 % 100 % 10
+
+#challenge 3
+
+movies = { wild: 1975,
+          man: 2004,
+          mind: 2001,
+          go: 1981 }
+
+puts movies[:wild]
+puts movies[:man]
+puts movies[:mind]
+puts movies[:go]
+
+#challenge 4
+
+dates = [1975, 2004, 2013 ,2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+#challenge 5
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#challenge 6
+
+puts 4.30 * 4.30
+puts 6.13 * 6.13
+puts 124.34 * 124.34
+
+
+
